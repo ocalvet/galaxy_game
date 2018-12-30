3410a060254ad163c6b16530747eb0ea73440b28
Ovidio R. Calvet <calveto@gmail.com>
2018-12-30 14:35:46 -0500

    Add green rectangle

--- lib/galaxy_game.dart
+++ lib/galaxy_game.dart
@@ -11,6 +11,10 @@ class GalaxyGame extends BaseGame {
     String text = 'Galaxy Game';
     TextPainter p = Flame.util
         .text(text, color: Colors.white, fontSize: 48.0);
+    Paint paint = Paint();
+    paint.color = Color(0xFF00FF00);
+    Rect rect = Rect.fromLTWH(100, 200, 200, 400);
+    canvas.drawRect(rect, paint);
     p.paint(canvas,
         new Offset(size.width - p.width - 10, size.height - p.height - 10));
     }
