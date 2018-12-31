f9d9ea6194a819b72e8714052100c8d1ee81f593
Ovidio R. Calvet <calveto@gmail.com>
2018-12-30 21:26:14 -0500

    make the space ship fly through the screen

--- lib/space_ship.dart
+++ lib/space_ship.dart
@@ -1,14 +1,17 @@
 import 'package:flame/components/component.dart';
 
+const SPEED = 100;
+
 class SpaceShip extends SpriteComponent {
+
   SpaceShip() : super.square(128.0, 'space_ship.png') {
     x = 200;
     y = 100;
-    this.angle = 0.0;
+    angle = 0.0;
   }
+
   @override
   void update(double t) {
-    y += t * 100;
-    // print(y);
+    y > 1000 ? y = -128 : y += t * SPEED;
   }
 }
